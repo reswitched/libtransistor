45b089d4d099779c8b67b5d3e4858d9e6e8e468d
3Daniel <daniel@dan3d.org>
2018-01-12 06:58:22 +1300

    Added initial Doxygen comments

--- include/libtransistor/gpu/gpu.h
+++ include/libtransistor/gpu/gpu.h
@@ -1,16 +1,59 @@
+/**
+ * @file libtransistor/gpu/gpu.h
+ * @brief GPU functions
+ */
+
 #pragma once
 
 #include<libtransistor/types.h>
 
+
+/**
+* @struct gpu_buffer_t
+* @brief Buffer to be used for various GPU functions
+*/
 typedef struct {
-	uint32_t nvmap_handle;
+	uint32_t nvmap_handle; ///< Handle for nvmap gpu memory management driver
 	size_t size;
 	uint32_t alignment;
 	uint8_t kind;
 } gpu_buffer_t;
 
+/**
+* @brief Initialize GPU
+*/
 result_t gpu_initialize();
+
+/**
+* @brief Initializes a GPU buffer with the args provided
+*
+* @param gpu_b GPU buffer
+* @param addr Description here...
+* @param size Buffer size
+* @param heapmask Description here...
+* @param flags Description here...
+* @param alignment Description here...
+* @param kind Description here...
+*/
 result_t gpu_buffer_initialize(gpu_buffer_t *gpu_b, void *addr, size_t size, uint32_t heapmask, uint32_t flags, uint32_t alignment, uint8_t kind);
+
+/**
+* @brief Get a GPU buffer
+*
+* @param gpu_b GPU buffer
+* @param id Description here...
+*/
 result_t gpu_buffer_get_id(gpu_buffer_t *gpu_b, uint32_t *id);
+
+/**
+* @brief Set up the GPU buffer with some defaults
+*
+* @param gpu_b GPU buffer
+* @param id Description here...
+*/
 result_t gpu_buffer_initialize_from_id(gpu_buffer_t *gpu_b, uint32_t id);
+
+/**
+* @brief Finalize GPU
+*/
 void gpu_finalize();
--- include/libtransistor/gpu/nv_ioc.h
+++ include/libtransistor/gpu/nv_ioc.h
@@ -1,3 +1,8 @@
+/**
+ * @file libtransistor/gpu/nv_ioc.h
+ * @brief NVMAP_IOC data structures
+ */
+
 #pragma once
 
 #define NVMAP_IOC_CREATE 0xC0080101
@@ -6,33 +11,63 @@
 #define NVMAP_IOC_PARAM 0xC00C0109
 #define NVMAP_IOC_GET_ID 0xC008010E
 
+/**
+* @struct nvmap_ioc_create_args
+* @brief Args to create an nvmap object 
+*
+*        Identical to Linux driver.
+*/
 typedef struct {
-	uint32_t size; // in
-	uint32_t handle; // out
+	uint32_t size; ///< In
+	uint32_t handle; ///< Out
 } nvmap_ioc_create_args;
 
+/**
+* @struct nvmap_ioc_from_id_args
+* @brief Args to get the handle to an existing nvmap object
+*
+*        Identical to Linux driver.
+*/
 typedef struct {
-	uint32_t id; // in
-	uint32_t handle; // out
+	uint32_t id; ///< In
+	uint32_t handle; ///< Out
 } nvmap_ioc_from_id_args;
 
+/**
+* @struct nvmap_ioc_alloc_args
+* @brief Memory allocation args structure for the nvmap object. 
+*
+*        Nintendo extended this one with 16 bytes, and changed it from in to inout.
+*/
 typedef struct {
 	uint32_t handle;
 	uint32_t heapmask;
-	uint32_t flags;
+	uint32_t flags; ///< (0=read-only, 1=read-write)
 	uint32_t align;
 	uint8_t kind;
 	uint8_t pad[7];
 	uint64_t addr;
 } nvmap_ioc_alloc_args;
 
+/**
+* @struct nvmap_ioc_param_args
+* @brief Info about a nvmap object. 
+*
+*        Identical to Linux driver, but extended with further params.
+*/
 typedef struct {
 	uint32_t handle;
-	uint32_t param;
+	uint32_t param; ///< // 1=SIZE, 2=ALIGNMENT, 3=BASE (returns error), 4=HEAP (always 0x40000000), 5=KIND, 6=COMPR (unused)
 	uint32_t value;
 } nvmap_ioc_param_args;
 
+/**
+* @struct nvmap_ioc_get_id_args
+* @brief An id for a nvmap object. 
+*
+*        Identical to Linux driver.
+*/
 typedef struct {
-	uint32_t id; // out
-	uint32_t handle; // in
+	uint32_t id; ///< Out ~0 indicates error
+	uint32_t handle; ///< In
 } nvmap_ioc_get_id_args;
