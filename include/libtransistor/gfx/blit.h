85ee1f4a85abdf6df00aeb61d5d419c1c8adf0a2
3Daniel <daniel@dan3d.org>
2018-01-12 07:05:42 +1300

    Added initial Doxygen comments

--- include/libtransistor/gfx/blit.h
+++ include/libtransistor/gfx/blit.h
@@ -1,3 +1,18 @@
+/**
+ * @file libtransistor/gfx/blit.h
+ * @brief Functions for graphics blits
+ */
+
 #pragma once
 
+/**
+* @brief An IPC request with default values set
+*
+* @param framebuffer Pointer to the framebuffer
+* @param image Description here...
+* @param w Width
+* @param h Height
+* @param tx Texture X
+* @param ty Texture Y
+*/
 void gfx_slow_swizzling_blit(uint32_t *framebuffer, uint32_t *image, int w, int h, int tx, int ty);
