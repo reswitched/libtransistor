83289edf9a109a0a6f4d390b77c1882caa14c3e0
Corentin Garcia <corenting@gmail.com>
2018-01-17 19:34:57 +0100

    Use python3 for elf2nxo

--- libtransistor.mk
+++ libtransistor.mk
@@ -19,7 +19,7 @@ AR_FLAGS := rcs
 # for compatiblity
 CFLAGS := $(CC_FLAGS)
 AS_FLAGS := -arch=aarch64 -triple aarch64-none-switch
-PYTHON2 := python2
+PYTHON3 := python3
 MEPHISTO := ctu
 RUBY := ruby
 COMPILER_RT_BUILTINS_LIB := $(LIBTRANSISTOR_HOME)/build/compiler-rt/lib/linux/libclang_rt.builtins-aarch64.a
@@ -30,7 +30,7 @@ LIBTRANSISTOR_NRO_LDFLAGS := --whole-archive $(LIBTRANSISTOR_NRO_LIB) --no-whole
 LIBTRANSISTOR_NSO_LDFLAGS := --whole-archive $(LIBTRANSISTOR_NSO_LIB) --no-whole-archive $(LIBTRANSISTOR_COMMON_LIBS)
 
 %.nro: %.nro.so
-	$(PYTHON2) $(LIBTRANSISTOR_HOME)/tools/elf2nxo.py $< $@ nro
+	$(PYTHON3) $(LIBTRANSISTOR_HOME)/tools/elf2nxo.py $< $@ nro
 
 %.nso: %.nso.so
-	$(PYTHON2) $(LIBTRANSISTOR_HOME)/tools/elf2nxo.py $< $@ nso
+	$(PYTHON3) $(LIBTRANSISTOR_HOME)/tools/elf2nxo.py $< $@ nso
