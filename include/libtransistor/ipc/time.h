9373e4223cb96527dbf093baef5c93d61c747826
misson20000 <xenotoad@xenotoad.net>
2018-01-27 13:32:57 -0800

    cleanup time module:
    - add system_clock_t for better abstraction
    - "system clock" is two words
    - variable declarations in header files should be extern
    - DRY
    - clean up properly in error cases
    - clean up properly in time_force_finalize

--- include/libtransistor/ipc/time.h
+++ include/libtransistor/ipc/time.h
@@ -7,9 +7,11 @@
 
 #include<libtransistor/types.h>
 
-ipc_object_t time_systemclock_user;
-ipc_object_t time_systemclock_network;
-ipc_object_t time_systemclock_local;
+typedef struct system_clock_t system_clock_t;
+
+extern system_clock_t *time_system_clock_user;
+extern system_clock_t *time_system_clock_network;
+extern system_clock_t *time_system_clock_local;
 
 /**
 * @brief Initialize Time service
@@ -17,14 +19,14 @@ ipc_object_t time_systemclock_local;
 result_t time_init();
 
 /**
-* @brief Get the current system time from one of the systemclocks
+* @brief Get the current system time from one of the system clocks
 */
-result_t time_systemclock_get_current_time(ipc_object_t systemclock, uint64_t *time);
+result_t time_system_clock_get_current_time(system_clock_t *system_clock, uint64_t *time);
 
 /**
-* @brief Set the current system time for one of the systemclocks
+* @brief Set the current system time for one of the system clocks
 */
-result_t time_systemclock_set_current_time(ipc_object_t systemclock, uint64_t *time);
+result_t time_system_clock_set_current_time(system_clock_t *system_clock, uint64_t time);
 
 /**
 * @brief Finalize Time service
--- lib/ipc/time.c
+++ lib/ipc/time.c
@@ -3,16 +3,32 @@
 #include<libtransistor/ipc/nifm.h>
 #include<libtransistor/ipc/sm.h>
 
-ipc_object_t time_systemclock_user;
-ipc_object_t time_systemclock_network;
-ipc_object_t time_systemclock_local;
+typedef struct system_clock_t {
+	ipc_object_t object;
+} system_clock_t;
+
+static system_clock_t sc_user, sc_network, sc_local;
+
+system_clock_t *time_system_clock_user;
+system_clock_t *time_system_clock_network;
+system_clock_t *time_system_clock_local;
 
 static int time_initializations = 0;
 
-result_t time_init() {
-	result_t r;
+static result_t open_clock(ipc_object_t *time_object, system_clock_t *clock, uint32_t request_id) {
 	ipc_request_t rq = ipc_default_request;
 	ipc_response_fmt_t rs = ipc_default_response_fmt;
+	
+	rq.request_id = request_id;
+	
+	rs.num_objects = 1;
+	rs.objects = &clock->object;
+	
+	return ipc_send(*time_object, &rq, &rs);
+}
+
+result_t time_init() {
+	result_t r;
 	ipc_object_t time_object;
 
 	if(time_initializations++ > 0) {
@@ -35,77 +51,64 @@ result_t time_init() {
 		}
 	}
 
-	rq.request_id = 0;
-	rs.num_objects = 1;
-	rs.objects = &time_systemclock_user;
-	r = ipc_send(time_object, &rq, &rs);
-
-	if(r != RESULT_OK) {
+	if((r = open_clock(&time_object, &sc_user, 0)) != RESULT_OK) {
 		goto fail_time_object;
 	}
+	time_system_clock_user = &sc_user;
 
-	rq = ipc_default_request;
-	rs = ipc_default_response_fmt;
-
-	rq.request_id = 1;
-	rs.num_objects = 1;
-	rs.objects = &time_systemclock_network;
-	r = ipc_send(time_object, &rq, &rs);
-
-	if(r != RESULT_OK) {
-		goto fail_time_object;
+	if((r = open_clock(&time_object, &sc_network, 1)) != RESULT_OK) {
+		goto fail_clock_user;
 	}
+	time_system_clock_network = &sc_network;
 
-	rq = ipc_default_request;
-	rs = ipc_default_response_fmt;
-
-	rq.request_id = 4;
-	rs.num_objects = 1;
-	rs.objects = &time_systemclock_local;
-	r = ipc_send(time_object, &rq, &rs);
-
-	if(r != RESULT_OK) {
-		goto fail_time_object;
+	if((r = open_clock(&time_object, &sc_local, 4)) != RESULT_OK) {
+		goto fail_clock_network;
 	}
+	time_system_clock_local = &sc_local;
 
-	sm_finalize();
 	ipc_close(time_object);
+	sm_finalize();
 	return RESULT_OK;
 
-	fail_time_object:
+fail_clock_network:
+	ipc_close(sc_network.object);
+fail_clock_user:
+	ipc_close(sc_user.object);
+fail_time_object:
 	ipc_close(time_object);
-	fail_sm:
+fail_sm:
 	sm_finalize();
-	fail:
+fail:
 	time_initializations--;
 	return r;
 }
 
-result_t time_systemclock_get_current_time(ipc_object_t systemclock, uint64_t *time) {
+result_t time_system_clock_get_current_time(system_clock_t *system_clock, uint64_t *time) {
 	ipc_request_t rq = ipc_default_request;
 	ipc_response_fmt_t rs = ipc_default_response_fmt;
 
 	rq.request_id = 0;
-	rs.raw_data = time;
+	rs.raw_data = (uint32_t*) time;
 	rs.raw_data_size = sizeof(*time);
 
-	return ipc_send(systemclock, &rq, &rs);
+	return ipc_send(system_clock->object, &rq, &rs);
 }
 
-result_t time_systemclock_set_current_time(ipc_object_t systemclock, uint64_t *time) {
+result_t time_system_clock_set_current_time(system_clock_t *system_clock, uint64_t time) {
 	ipc_request_t rq = ipc_default_request;
 	ipc_response_fmt_t rs = ipc_default_response_fmt;
 
 	rq.request_id = 1;
-	rq.raw_data = &time;
-	rq.raw_data_size = sizeof(*time);
+	rq.raw_data = (uint32_t*) &time;
+	rq.raw_data_size = sizeof(time);
 
-	return ipc_send(systemclock, &rq, &rs);
+	return ipc_send(system_clock->object, &rq, &rs);
 }
 
 static void time_force_finalize() {
-	ipc_close(time_systemclock_user);
-	ipc_close(time_systemclock_network);
+	ipc_close(sc_user.object);
+	ipc_close(sc_network.object);
+	ipc_close(sc_local.object);
 	time_initializations = 0;
 }
 
