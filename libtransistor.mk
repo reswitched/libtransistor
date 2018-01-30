6c52d73240cfa3b4e08ecdac3c4c4282d22698b3
misson20000 <xenotoad@xenotoad.net>
2018-01-29 23:51:21 -0800

    don't break compatibility with older Makefiles

--- libtransistor.mk
+++ libtransistor.mk
@@ -45,15 +45,18 @@ LIB_DEP_NEWLIB_LIBC := $(LIBTRANSISTOR_HOME)/build/newlib/aarch64-none-switch/ne
 LIB_DEP_NEWLIB_LIBM := $(LIBTRANSISTOR_HOME)/build/newlib/aarch64-none-switch/newlib/libm.a
 LIB_DEP_PTHREAD := $(LIBTRANSISTOR_HOME)/build/pthread/libpthread.a
 LIBTRANSISTOR_COMMON_LIB_DEPS := $(LIB_DEP_NEWLIB_LIBC) $(LIB_DEP_NEWLIB_LIBM) $(LIB_DEP_COMPILER_RT_BUILTINS) $(LIB_DEP_SDL2) $(LIB_DEP_PTHREAD)
+LIBTRANSISTOR_COMMON_LIBS := $(LIBTRANSISTOR_COMMON_LIB_DEPS) # for older Makefiles
 LIBTRANSISTOR_NRO_DEP := $(LIBTRANSISTOR_HOME)/build/lib/libtransistor.nro.a
 LIBTRANSISTOR_NSO_DEP := $(LIBTRANSISTOR_HOME)/build/lib/libtransistor.nso.a
+LIBTRANSISTOR_NRO_LIB := $(LIBTRANSISTOR_NRO_DEP)
+LIBTRANSISTOR_NSO_LIB := $(LIBTRANSISTOR_NSO_DEP)
 LIBTRANSISTOR_NRO_DEPS := $(LIBTRANSISTOR_HOME)/build/lib/libtransistor.nro.a $(LIBTRANSISTOR_COMMON_LIB_DEPS)
 LIBTRANSISTOR_NSO_DEPS := $(LIBTRANSISTOR_HOME)/build/lib/libtransistor.nso.a $(LIBTRANSISTOR_COMMON_LIB_DEPS)
 
-LIBTRANSISTOR_COMMON_LIBS := -lc -lm -lclang_rt.builtins-aarch64 -lSDL2 -lpthread
+LIBTRANSISTOR_COMMON_LDFLAGS := -lc -lm -lclang_rt.builtins-aarch64 -lSDL2 -lpthread
 
-LIBTRANSISTOR_NRO_LDFLAGS := --whole-archive -ltransistor.nro --no-whole-archive $(LIBTRANSISTOR_COMMON_LIBS)
-LIBTRANSISTOR_NSO_LDFLAGS := --whole-archive -ltransistor.nso --no-whole-archive $(LIBTRANSISTOR_COMMON_LIBS)
+LIBTRANSISTOR_NRO_LDFLAGS := --whole-archive -ltransistor.nro --no-whole-archive $(LIBTRANSISTOR_COMMON_LDFLAGS)
+LIBTRANSISTOR_NSO_LDFLAGS := --whole-archive -ltransistor.nso --no-whole-archive $(LIBTRANSISTOR_COMMON_LDFLAGS)
 
 %.nro: %.nro.so
 	$(PYTHON3) $(LIBTRANSISTOR_HOME)/tools/elf2nxo.py $< $@ nro
