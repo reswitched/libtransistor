fd569ccaddcdffc2495f2c0cd0ba667df17362b0
roblabla <unfiltered@roblab.la>
2018-02-24 00:55:16 +0100

    Implement file reading

--- lib/fs/fspfs.c
+++ lib/fs/fspfs.c
@@ -1,9 +1,13 @@
 #include <libtransistor/types.h>
 #include <libtransistor/err.h>
+#include <libtransistor/fd.h>
 #include <libtransistor/fs/fspfs.h>
 #include <libtransistor/ipc/fs/ifilesystem.h>
 #include <string.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <libtransistor/util.h>
 
 struct inode {
 	bool is_dir;
@@ -12,6 +16,13 @@ struct inode {
 	char name[0x301];// TODO: Static ? PTR ?
 };
 
+struct ifs_file {
+	ifile_t file;
+	off_t head;
+};
+
+
+static struct file_operations fspfs_file_ops;
 static trn_inode_ops_t fspfs_inode_ops;
 static trn_dir_ops_t trn_fspfs_dir_ops;
 
@@ -57,13 +68,51 @@ fail:
 	return r;
 }
 
-static result_t fspfs_open_as_file(void *data, int mode, int *fd) {
-	return LIBTRANSISTOR_ERR_FS_INTERNAL_ERROR;
+static result_t fspfs_open_as_file(void *data, int flags, int *fd) {
+	struct inode *inode = (struct inode*)data;
+	result_t r;
+	uint32_t ifs_flags;
+
+	if (inode->is_dir)
+		return LIBTRANSISTOR_ERR_FS_NOT_A_FILE;
+
+	if (flags & O_RDWR)
+		ifs_flags = 3;
+	else if (flags & O_WRONLY)
+		ifs_flags = 2;
+	else
+		ifs_flags = 1;
+	if (flags & O_APPEND)
+		ifs_flags |= 4;
+
+	struct ifs_file *f = malloc(sizeof(struct ifs_file));
+	if (f == NULL)
+		return LIBTRANSISTOR_ERR_OUT_OF_MEMORY;
+
+	printf("ifilesystem_open_file %s\n", inode->name);
+	if ((r = ifilesystem_open_file(inode->fs, &f->file, ifs_flags, inode->name)) != RESULT_OK)
+		goto fail;
+	f->head = 0;
+
+	*fd = fd_create_file(&fspfs_file_ops, f);
+	if (*fd < 0)
+		goto fail_ifs;
+
+	return RESULT_OK;
+
+fail_ifs:
+	ipc_close(f->file);
+fail:
+	free(f);
+	return r;
 }
 
 static result_t fspfs_open_as_dir(void *data, trn_dir_t *out) {
 	struct inode *inode = (struct inode*)data;
 	result_t r;
+
+	if (!inode->is_dir)
+		return LIBTRANSISTOR_ERR_FS_NOT_A_DIRECTORY;
 
 	idirectory_t *dir = malloc(sizeof(idirectory_t));
 	if (dir == NULL)
@@ -117,6 +166,55 @@ static void trn_fspfs_dir_close(void *data) {
 	ipc_close(*dir);
 	free(dir);
 }
+
+static off_t fspfs_file_llseek(void *data, off_t offset, int whence) {
+	struct ifs_file *file = data;
+	switch(whence) {
+	case SEEK_SET:
+		file->head = offset;
+		break;
+	case SEEK_CUR:
+		file->head+= offset;
+		break;
+	case SEEK_END:
+		// TODO: Implement.
+	default:
+		return -EINVAL;
+	}
+	return file->head;
+}
+
+static ssize_t fspfs_file_read(void *data, char *buf, size_t buf_size) {
+	struct ifs_file *f = data;
+	uint64_t out_size;
+	result_t r;
+
+	printf("Reading file from %x to %x\n", f->head, f->head + buf_size);
+	if ((r = ifile_read(f->file, &out_size, buf, buf_size, 0, f->head, buf_size)) != RESULT_OK) {
+		printf("Got an error: %x\n", r);
+		return -EIO;
+	}
+	printf("Successfuly read from file: %lu\n", out_size);
+	hexdump(buf, out_size);
+	f->head += out_size;
+	return out_size;
+}
+
+static int fspfs_file_release(struct file *file) {
+	struct ifs_file *f = file->data;
+
+	ipc_close(f->file);
+	free(f);
+	return 0;
+}
+
+static struct file_operations fspfs_file_ops = {
+	.llseek = fspfs_file_llseek,
+	.read = fspfs_file_read,
+	.write = NULL,
+	.flush = NULL,
+	.release = fspfs_file_release
+};
 
 static trn_dir_ops_t trn_fspfs_dir_ops = {
 	.next = trn_fspfs_dir_next,
