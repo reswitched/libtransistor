26a3d993ab7e2c21983bdde32681ad1b610edbb9
roblabla <unfiltered@roblab.la>
2018-02-27 01:45:23 +0100

    Writing works

--- lib/ipc/fs/ifile.c
+++ lib/ipc/fs/ifile.c
@@ -46,16 +46,16 @@ result_t ifile_read(ifile_t obj, uint64_t *out_out_size, int8_t * out_out_buf, s
 	return res;
 }
 
-result_t ifile_write(ifile_t obj, uint64_t in_unk0, uint64_t in_offset, uint32_t in_size, const int8_t * in_buf, size_t in_buf_size) {
+result_t ifile_write(ifile_t obj, uint32_t in_unk0, uint64_t in_offset, uint64_t in_size, const int8_t * in_buf, size_t in_buf_size) {
 	result_t res;
 	ipc_request_t rq = ipc_default_request;
 	rq.request_id = 1;
 
-	uint8_t raw[20];
+	uint8_t raw[24];
 	rq.raw_data = raw;
 	rq.raw_data_size = 20;
 
-	*(uint64_t*)(raw + 0) = in_unk0;
+	*(uint32_t*)(raw + 0) = in_unk0;
 	*(uint64_t*)(raw + 8) = in_offset;
 	*(uint32_t*)(raw + 16) = in_size;
 	ipc_buffer_t in_buf_buf = {
