401ed8eeead781d3812d4193bde7848a042d6104
misson20000 <xenotoad@xenotoad.net>
2018-05-16 12:06:29 -0700

    fix resolving symbols in non-global modules

--- lib/ld/resolve.c
+++ lib/ld/resolve.c
@@ -11,8 +11,8 @@
 #include<stdlib.h>
 #include<string.h>
 
-static result_t ld_try_resolve_symbol(module_t *try_mod, const char *find_name, uint64_t find_name_hash, Elf64_Sym **def, module_t **defining_module) {
-	if(!try_mod->input.is_global) { return LIBTRANSISTOR_ERR_TRNLD_COULD_NOT_RESOLVE_SYMBOL; }
+static result_t ld_try_resolve_symbol(module_t *try_mod, const char *find_name, uint64_t find_name_hash, Elf64_Sym **def, module_t **defining_module, bool require_global) {
+	if(require_global && !try_mod->input.is_global) { return LIBTRANSISTOR_ERR_TRNLD_COULD_NOT_RESOLVE_SYMBOL; }
 	if(try_mod->symtab == NULL) { return LIBTRANSISTOR_ERR_TRNLD_COULD_NOT_RESOLVE_SYMBOL; }
 	if(try_mod->strtab == NULL) { return LIBTRANSISTOR_ERR_TRNLD_COULD_NOT_RESOLVE_SYMBOL; }
 	if(try_mod->hash != NULL) {
@@ -40,7 +40,7 @@ static result_t ld_try_resolve_symbol(module_t *try_mod, const char *find_name, 
 result_t ld_resolve_load_symbol(const char *find_name, Elf64_Sym **def, module_t **defining_module) {
 	uint64_t find_name_hash = elf_hash_string(find_name);
 	trn_list_foreach(&ld_module_list_head, i) {
-		result_t r = ld_try_resolve_symbol(trn_list_entry(module_list_node_t, list, i)->module, find_name, find_name_hash, def, defining_module);
+		result_t r = ld_try_resolve_symbol(trn_list_entry(module_list_node_t, list, i)->module, find_name, find_name_hash, def, defining_module, true);
 		if(r == LIBTRANSISTOR_ERR_TRNLD_COULD_NOT_RESOLVE_SYMBOL) {
 			continue;
 		} else { // this includes RESULT_OK
@@ -53,12 +53,12 @@ result_t ld_resolve_load_symbol(const char *find_name, Elf64_Sym **def, module_t
 result_t ld_resolve_dependency_symbol(module_t *find_mod, const char *find_name, Elf64_Sym **def, module_t **defining_module) {
 	uint64_t find_name_hash = elf_hash_string(find_name);
 	result_t r;
-	r = ld_try_resolve_symbol(find_mod, find_name, find_name_hash, def, defining_module);
+	r = ld_try_resolve_symbol(find_mod, find_name, find_name_hash, def, defining_module, false);
 	if(r != LIBTRANSISTOR_ERR_TRNLD_COULD_NOT_RESOLVE_SYMBOL) { // this includes RESULT_OK
 		return r;
 	}
 	trn_list_foreach(&find_mod->dependencies, i) {
-		r = ld_try_resolve_symbol(trn_list_entry(module_list_node_t, list, i)->module, find_name, find_name_hash, def, defining_module);
+		r = ld_try_resolve_symbol(trn_list_entry(module_list_node_t, list, i)->module, find_name, find_name_hash, def, defining_module, false);
 		if(r == LIBTRANSISTOR_ERR_TRNLD_COULD_NOT_RESOLVE_SYMBOL) {
 			continue;
 		} else { // this includes RESULT_OK
