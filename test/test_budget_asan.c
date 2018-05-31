4548ae62e5e2fa6a5ad12e13fb5930ab25dc9676
misson20000 <xenotoad@xenotoad.net>
2018-05-30 22:10:47 -0700

    remove a few tests that I accidentally committed at some point

--- test/test_budget_asan.c
+++
@@ -1,8 +0,0 @@
-#include<stdint.h>
-#include<malloc.h>
-
-int main(int argc, char *argv[]) {
-	uint32_t *ptr = malloc(4);
-	*ptr = 8;
-	return *ptr;
-}
--- test/test_malloc_stress.c
+++
@@ -1,9 +0,0 @@
-#include<malloc.h>
-#include<stdio.h>
-
-int main(int argc) {
-	void *p = NULL;
-	do {
-		printf(" -> %p\n", p = malloc(0x2000000));
-	} while(p != NULL);
-}
