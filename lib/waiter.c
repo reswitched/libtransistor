97baa01f62bd0be03679847fa6649aa89da6dc58
misson20000 <xenotoad@xenotoad.net>
2018-08-31 17:06:30 -0700

    waiter: use recursive mutex, use interrupt lock, and handle svcWaitSynchronization interrupt result

--- lib/waiter.c
+++ lib/waiter.c
@@ -21,7 +21,7 @@ struct wait_record_t {
 };
 
 struct waiter_t {
-	trn_mutex_t mutex;
+	trn_recursive_mutex_t mutex;
 	wait_record_t list;
 	
 	size_t handle_buffer_length;
@@ -38,7 +38,7 @@ waiter_t *waiter_create() {
 	}
 	
 	memset(waiter, 0, sizeof(*waiter));
-	trn_mutex_create(&waiter->mutex);
+	trn_recursive_mutex_create(&waiter->mutex);
 	return waiter;
 }
 
@@ -54,14 +54,14 @@ wait_record_t *waiter_add(waiter_t *waiter, handle_t handle, bool (*signal)(void
 	record->signal = signal;
 	record->age = 0;
 
-	trn_mutex_interrupt_lock(&waiter->mutex);
+	trn_recursive_mutex_interrupt_lock(&waiter->mutex);
 	record->prev = &waiter->list;
 	record->next = waiter->list.next;
 	if(record->next) {
 		record->next->prev = record;
 	}
 	waiter->list.next = record;
-	trn_mutex_unlock(&waiter->mutex);
+	trn_recursive_mutex_unlock(&waiter->mutex);
 
 	return record;
 }
@@ -78,13 +78,13 @@ static int record_compare(const void *va, const void *vb) {
 
 result_t waiter_wait(waiter_t *waiter, uint64_t timeout) {
 	size_t num_records = 0;
-	trn_mutex_lock(&waiter->mutex);
+	trn_recursive_mutex_lock(&waiter->mutex);
 	for(wait_record_t *record = waiter->list.next; record != NULL; record = record->next) {
 		if(num_records >= waiter->record_buffer_length) {
 			size_t target_record_buffer_length = (num_records + 1) * 2;
 			void *new_record_buffer = realloc(waiter->record_buffer, target_record_buffer_length * sizeof(*waiter->record_buffer));
 			if(new_record_buffer == NULL) {
-				trn_mutex_unlock(&waiter->mutex);
+				trn_recursive_mutex_unlock(&waiter->mutex);
 				return LIBTRANSISTOR_ERR_OUT_OF_MEMORY;
 			}
 			waiter->record_buffer_length = target_record_buffer_length;
@@ -99,7 +99,7 @@ result_t waiter_wait(waiter_t *waiter, uint64_t timeout) {
 	if(num_records > waiter->handle_buffer_length) {
 		void *new_handle_buffer = realloc(waiter->handle_buffer, num_records * sizeof(*waiter->handle_buffer));
 		if(new_handle_buffer == NULL) {
-			trn_mutex_unlock(&waiter->mutex);
+			trn_recursive_mutex_unlock(&waiter->mutex);
 			return LIBTRANSISTOR_ERR_OUT_OF_MEMORY;
 		}
 		waiter->handle_buffer_length = num_records;
@@ -137,23 +137,23 @@ result_t waiter_wait(waiter_t *waiter, uint64_t timeout) {
 		} else {
 			signalled_record->is_being_signalled = false;
 		}
-		trn_mutex_unlock(&waiter->mutex);
+		trn_recursive_mutex_unlock(&waiter->mutex);
 		return RESULT_OK;
-	} else if(r == 0xea01) { // timeout
+	} else if(r == 0xea01 || r == 0xec01) { // timeout or interrupt
 		// touch all records
 		for(size_t i = 0; i < num_records; i++) {
 			waiter->record_buffer[i]->age = 0;
 		}
-		trn_mutex_unlock(&waiter->mutex);
+		trn_recursive_mutex_unlock(&waiter->mutex);
 		return RESULT_OK;
 	} else {
-		trn_mutex_unlock(&waiter->mutex);
+		trn_recursive_mutex_unlock(&waiter->mutex);
 		return r;
 	}
 }
 
 void waiter_cancel(waiter_t *waiter, wait_record_t *record) {
-	trn_mutex_lock(&waiter->mutex);
+	trn_recursive_mutex_interrupt_lock(&waiter->mutex);
 	if(record->is_being_signalled) {
 		// defer unlink+free until after signal handler exits
 		record->destroy_flag = true;
@@ -164,11 +164,11 @@ void waiter_cancel(waiter_t *waiter, wait_record_t *record) {
 		}
 		free(record);
 	}
-	trn_mutex_unlock(&waiter->mutex);
+	trn_recursive_mutex_unlock(&waiter->mutex);
 }
 
 void waiter_destroy(waiter_t *waiter) {
-	trn_mutex_lock(&waiter->mutex);
+	trn_recursive_mutex_interrupt_lock(&waiter->mutex);
 	for(wait_record_t *record = waiter->list.next; record != NULL;) {
 		wait_record_t *next = record->next;
 		free(record);
@@ -180,6 +180,6 @@ void waiter_destroy(waiter_t *waiter) {
 	if(waiter->record_buffer != NULL) {
 		free(waiter->record_buffer);
 	}
-	trn_mutex_unlock(&waiter->mutex);
+	trn_recursive_mutex_unlock(&waiter->mutex);
 	free(waiter);
 }
