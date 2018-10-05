0a1699c85d7e5e759e5cc41ae261173b61d0b60a
misson20000 <xenotoad@xenotoad.net>
2018-10-04 18:52:10 -0700

    fix version detection for 5.0.0

--- lib/environment.c
+++ lib/environment.c
@@ -7,12 +7,17 @@ void *env_stack_top;
 kernel_version_t env_get_kernel_version() {
 	static kernel_version_t version = KERNEL_VERSION_INVALID;
 	if(version == KERNEL_VERSION_INVALID) {
-		version = KERNEL_VERSION_500;
+		version = KERNEL_VERSION_MAX;
 		uint64_t info;
-		if(svcGetInfo(&info, 20, 0xffff8001, 0) == 0xf001) { version = KERNEL_VERSION_500 - 1; }
-		if(svcGetInfo(&info, 19, 0xffff8001, 0) == 0xf001) { version = KERNEL_VERSION_400 - 1; }
-		if(svcGetInfo(&info, 16, 0xffff8001, 0) == 0xf001) { version = KERNEL_VERSION_300 - 1; }
-		if(svcGetInfo(&info, 12, 0xffff8001, 0) == 0xf001) { version = KERNEL_VERSION_200 - 1; }
+		result_t r;
+		if((r = svcGetInfo(&info, 20, 0xffffffff, 0)) == 0xf001) { version = KERNEL_VERSION_500 - 1; }
+		if((r = svcGetInfo(&info, 19, 0xffffffff, 0)) == 0xf001) { // removed in 5.0.0
+			if(version != KERNEL_VERSION_MAX) {
+				version = KERNEL_VERSION_400 - 1;
+			}
+		}
+		if((r = svcGetInfo(&info, 16, 0xffffffff, 0)) == 0xf001) { version = KERNEL_VERSION_300 - 1; }
+		if((r = svcGetInfo(&info, 12, 0xffffffff, 0)) == 0xf001) { version = KERNEL_VERSION_200 - 1; }
 	}
 	return version;
 }
